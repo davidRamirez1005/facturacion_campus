48bd63901b060da1b08c2c485ccb12dbc8bec880
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-06-09 21:33:57 -0500

    feat: :sparkles: funcionalidad metodo get
    cuando busca por cedula los datos se agregan a los inputs correspondientes

--- index.php
+++ index.php
@@ -1,47 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Facturación Campus</title>
-    <link rel="stylesheet" href="./styles.css">
-</head>
-<body>
-<form action="index.php" method="POST">
-        <div class="content">
-            <div class="fila">
-                <div class="columna">
-                    <input type="text" placeholder="NOMBRE" name="nombre" value="<?php echo isset($_SESSION['nombre']) ? $_SESSION['nombre'] : ''; ?>">
-                    <input type="text" placeholder="APELLIDOS" name="apellidos" value="<?php echo isset($_SESSION['apellidos']) ? $_SESSION['apellidos'] : ''; ?>">
-                    <input type="text" placeholder="DIRECCIÓN" name="direccion" value="<?php echo isset($_SESSION['direccion']) ? $_SESSION['direccion'] : ''; ?>">
-                </div>
-                <div class="columna">
-                    <h1 class="campus">CAMPUSLANDS</h1>
-                    <input type="number" placeholder="EDAD" name="edad" value="<?php echo isset($_SESSION['edad']) ? $_SESSION['edad'] : ''; ?>">
-                    <input type="email" placeholder="EMAIL" name="email" value="<?php echo isset($_SESSION['email']) ? $_SESSION['email'] : ''; ?>">
-                </div>
-            </div>
-            <br><br><br>
-            <div class="fila">
-                <div class="columna">
-                    <input type="time" placeholder="HORARIO DE ENTRADA" name="horario" value="<?php echo isset($_SESSION['horario']) ? $_SESSION['horario'] : ''; ?>">
-                    <input type="number" placeholder="TEAM" name="team" value="<?php echo isset($_SESSION['team']) ? $_SESSION['team'] : ''; ?>">
-                    <input type="text" placeholder="ENTRENADOR" name="entrenador" value="<?php echo isset($_SESSION['entrenador']) ? $_SESSION['entrenador'] : ''; ?>">
-                </div>
-                <div class="columna">
-                    <button type="submit" name="guardar">Guardar</button>
-                    <button type="submit" name="editar">Editar</button>
-                    <button type="submit" name="buscar">buscar</button>
-                    <button>Eliminar</button>
-                    <input type="text" placeholder="CÉDULA" name="cedula" value="<?php echo isset($_SESSION['cedula']) ? $_SESSION['cedula'] : ''; ?>">
-                </div>
-            </div>
-            <br><br><br>
-        </div>
-    </form>
-</body>
-</html>
 <?php
 session_start();
 
@@ -116,28 +72,76 @@ if (isset($_POST['guardar'])) {
     // print_r($_DATA);
 
 
-    session_destroy();
+    // session_destroy();
 }else if (isset($_POST['buscar'])) {
-    $cedula2 = ($_POST['cedula']) ;
-    $_DATA2 = file_get_contents("https://648136c029fa1c5c50313005.mockapi.io/informacion/"."?cedula=".$cedula2);
+    $cedula = ($_POST['cedula']) ;
+    $_DATA2 = file_get_contents("https://648136c029fa1c5c50313005.mockapi.io/informacion/"."?cedula=".$cedula);
     
-    print_r($_DATA2);
-
+    
     $resultado = json_decode($_DATA2,true);
-       // Verificar si se encontraron datos para la cédula especificada
-       if (!empty($result)) {
-        $nombre = $result[0]['nombre'];
-        $apellidos = $result[0]['apellidos'];
-        $direccion = $result[0]['direccion'];
-        $edad = $result[0]['edad'];
-        $email = $result[0]['email'];
-        $horario = $result[0]['horario'];
-        $team = $result[0]['team'];
-        $entrenador = $result[0]['entrenador'];
+    $resultado = $resultado[0];
+    // Verificar si se encontraron datos para la cédula especificada
+    if (!empty($resultado)) {
+        $nombre = $resultado[0]['nombre'];
+        $apellidos = $resultado[0]['apellidos'];
+        $direccion = $resultado[0]['direccion'];
+        $edad = $resultado[0]['edad'];
+        $email = $resultado[0]['email'];
+        $horario = $resultado[0]['horario'];
+        $team = $resultado[0]['team'];
+        $entrenador = $resultado[0]['entrenador'];
     }
-};
-// else if (isset($_POST['editar'])){
+    // print_r($resultado);
     
-// };
+}else if(isset($_POST['editar'])){
+    $cedula = ($_POST['cedula']) ;
+    $_DATA3 = file_get_contents("https://648136c029fa1c5c50313005.mockapi.io/informacion/"."?cedula=".$cedula);
+}
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facturación Campus</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+<form action="index.php" method="POST">
+        <div class="content">
+            <div class="fila">
+                <div class="columna">
+                    <input type="text" placeholder="NOMBRE" name="nombre" value="<?php echo isset($resultado) ? $resultado["nombre"] : ''; ?>">
+                    <input type="text" placeholder="APELLIDOS" name="apellidos" value="<?php echo isset($resultado) ? $resultado["apellidos"] : ''; ?>">
+                    <input type="text" placeholder="DIRECCIÓN" name="direccion" value="<?php echo isset($resultado) ? $resultado["direccion"] : ''; ?>">
+                </div>
+                <div class="columna">
+                    <h1 class="campus">CAMPUSLANDS</h1>
+                    <input type="number" placeholder="EDAD" name="edad" value="<?php echo isset($resultado) ? $resultado["edad"] : ''; ?>">
+                    <input type="email" placeholder="EMAIL" name="email" value="<?php echo isset($resultado) ? $resultado["email"] : ''; ?>">
+                </div>
+            </div>
+            <br><br><br>
+            <div class="fila">
+                <div class="columna">
+                    <input type="time" placeholder="HORARIO DE ENTRADA" name="horario" value="<?php echo isset($resultado) ? $resultado["horario"] : ''; ?>">
+                    <input type="text" placeholder="TEAM" name="team" value="<?php echo isset($resultado) ? $resultado["team"] : ''; ?>">
+                    <input type="text" placeholder="ENTRENADOR" name="entrenador" value="<?php echo isset($resultado) ? $resultado["entrenador"] : ''; ?>">
+                </div>
+                <div class="columna">
+                    <button type="submit" name="guardar">Guardar</button>
+                    <button type="submit" name="editar">Editar</button>
+                    <button type="submit" name="buscar">buscar</button>
+                    <button type="submit" name="eliminar">Eliminar</button>
+                    <input type="text" placeholder="CÉDULA" name="cedula" value="<?php echo isset($resultado) ? $resultado["cedula"] : ''; ?>">
+                </div>
+            </div>
+            <br><br><br>
+        </div>
+    </form>
+</body>
+</html>
